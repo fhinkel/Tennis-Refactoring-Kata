87298fc5a05c3f774c48a8bb78e18d44171e04db
Franziska Hinkelmann <franzih@chromium.org>
2016-10-25 21:14:31 +0100

    Refactor #3

--- cpp/tennis3.cc
+++ cpp/tennis3.cc
@@ -1,17 +1,28 @@
 #include <string>
 
+const bool nobodyMoreThanForty(int score1, int score2) {
+  return score1 < 4 && score2 < 4;
+}
+
+const bool notDeuce(int score1, int score2) {
+  return score1 + score2 < 6;
+}
+
+const bool tied(int score1, int score2) {
+  return score1 == score2;
+}
+
+const bool oneOff(int score1, int score2) {
+  return (score1 - score2) * (score1 - score2) == 1;
+}
+
 const std::string tennis_score(int p1, int p2) {
-    std::string s;
-    std::string p1N = "player1";
-    std::string p2N = "player2";
-    if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6)) {
-        std::string p[4] = {"Love", "Fifteen", "Thirty", "Forty"}; 
-        s = p[p1];
-        return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-    } else {
-        if (p1 == p2)
-            return "Deuce";
-        s = p1 > p2 ? p1N : p2N;
-        return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
-    }
+  if (nobodyMoreThanForty(p1, p2) && notDeuce(p1, p2)) {
+    std::string p[4] = {"Love", "Fifteen", "Thirty", "Forty"};
+    return (tied(p1, p2)) ? p[p1] + "-All" : p[p1] + "-" + p[p2];
+  }
+  if (tied(p1, p2))
+    return "Deuce";
+  std::string player = p1 > p2 ? "player1" : "player2";
+  return oneOff(p1, p2) ? "Advantage " + player : "Win for " + player;
 }
