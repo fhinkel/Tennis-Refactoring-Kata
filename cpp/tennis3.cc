8118286bb6c20b7cdf113a8ec2589e8cd596fb43
Franziska Hinkelmann <franzih@chromium.org>
2016-10-25 21:15:51 +0100

    Refactor #3

--- cpp/tennis3.cc
+++ cpp/tennis3.cc
@@ -17,12 +17,13 @@ const bool oneOff(int score1, int score2) {
 }
 
 const std::string tennis_score(int p1, int p2) {
-  if (nobodyMoreThanForty(p1, p2) && notDeuce(p1, p2)) {
+  if (!nobodyMoreThanForty(p1, p2) || !notDeuce(p1, p2)) {
+    if (tied(p1, p2))
+      return "Deuce";
+    std::string player = p1 > p2 ? "player1" : "player2";
+    return oneOff(p1, p2) ? "Advantage " + player : "Win for " + player;
+  }
     std::string p[4] = {"Love", "Fifteen", "Thirty", "Forty"};
     return (tied(p1, p2)) ? p[p1] + "-All" : p[p1] + "-" + p[p2];
-  }
-  if (tied(p1, p2))
-    return "Deuce";
-  std::string player = p1 > p2 ? "player1" : "player2";
-  return oneOff(p1, p2) ? "Advantage " + player : "Win for " + player;
+  
 }
