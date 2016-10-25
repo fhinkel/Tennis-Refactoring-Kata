dbc2a4dcf968f550167d243e3bded4390be09d1b
Franziska Hinkelmann <franzih@chromium.org>
2016-10-25 20:28:04 +0100

    Basic refactoring

--- cpp/tennis1.cc
+++ cpp/tennis1.cc
@@ -20,36 +20,48 @@ const std::string namedScore(int score) {
     }
 }
 
-const bool moreThanForty()
+const bool moreThanForty(int s1, int s2) {
+    return s1 >= 4 || s2 >= 4;
+}
+
+const std::string announceMoreThanForty(std::string player, int diff) {
+    if (diff == 1) {
+        return "Advantage " + player;
+    } else {
+        return "Win for " + player;
+    }
+}
+
+const std::string announceRegularScore(int s1, int s2) {
+    return namedScore(s1) + "-" + namedScore(s2);
+}
+
+const std::string announceTiedScore(int score) {
+    switch (score)
+    {
+        case 0:
+            return "Love-All";
+        case 1:
+            return "Fifteen-All";
+        case 2:
+            return "Thirty-All";
+        default:
+            return "Deuce";
+    }
+}
 
 const std::string tennis_score(int p1Score, int p2Score) {
-    if (scoreIsTied(p1Score, p2Score))
-    {
-        switch (p1Score)
-        {
-            case 0:
-                return "Love-All";
-            case 1:
-                return "Fifteen-All";
-            case 2:
-                return "Thirty-All";
-            default:
-                return "Deuce";
-
+    if (scoreIsTied(p1Score, p2Score)) {
+        return announceTiedScore(p1Score);
+    }
+    if (moreThanForty(p1Score, p2Score)) {
+        if (p1Score > p2Score) {
+            return announceMoreThanForty("player1", p1Score - p2Score);
         }
+        return announceMoreThanForty("player2", p2Score - p1Score);
+
     }
-    else if (p1Score>=4 || p2Score>=4)
-    {
-        int minusResult = p1Score-p2Score;
-        if (minusResult==1) return "Advantage player1";
-        else if (minusResult ==-1) return "Advantage player2";
-        else if (minusResult>=2) return "Win for player1";
-        else return "Win for player2";
-    }
-    else
-    {
-        return namedScore(p1Score) + "-" + namedScore(p2Score);
-    }
+    return announceRegularScore(p1Score, p2Score);
 }
 
 
