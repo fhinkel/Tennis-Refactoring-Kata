e6216fd3185e4d8303ab1502a622a60ae4dcb213
Franziska Hinkelmann <franzih@chromium.org>
2016-10-25 20:59:21 +0100

    Refactor #2

--- cpp/tennis2.cc
+++ cpp/tennis2.cc
@@ -7,71 +7,38 @@ std::string regularScore(int score) {
     return "Fifteen";
   if (score == 2)
     return "Thirty";
-  if (score==3)
+  if (score == 3)
     return "Forty";
   return "Error!";
 }
 
 const std::string tennis_score(int p1Score, int p2Score) {
-    std::string score = "";
-    std::string P1res = "";
-    std::string P2res = "";
-    if (p1Score == p2Score) {
-      if (p1Score <= 2) {
-        return regularScore(p1Score) + "-All";
-      }
-      return "Deuce";
+  if (p1Score == p2Score) {
+    if (p1Score <= 2) {
+      return regularScore(p1Score) + "-All";
     }
-    if (p1Score > 0 && p2Score == 0)
-    {
-        P1res = regularScore(p1Score);
-        P2res = "Love";
-        score = P1res + "-" + P2res;
+    return "Deuce";
+  }
+
+  if (p1Score >= 4 || p2Score >= 4) {
+    if (p1Score - p2Score >= 2) {
+      return "Win for player1";
     }
-    if (p2Score > 0 && p1Score==0)
-    {
-        P2res = regularScore(p2Score);
-        P1res = "Love";
-        score = P1res + "-" + P2res;
+    if (p2Score - p1Score >= 2) {
+      return "Win for player2";
     }
-    
-    if (p1Score>p2Score && p1Score < 4)
-    {
-        P1res = regularScore(p1Score);
-        P2res = regularScore(p2Score);
-        score = P1res + "-" + P2res;
+
+    if (p1Score > p2Score) {
+      return "Advantage player1";
     }
-    if (p2Score>p1Score && p2Score < 4)
-    {
-        if (p2Score==2)
-            P2res="Thirty";
-        if (p2Score==3)
-            P2res="Forty";
-        if (p1Score==1)
-            P1res="Fifteen";
-        if (p1Score==2)
-            P1res="Thirty";
-        score = P1res + "-" + P2res;
+
+    if (p2Score > p1Score) {
+      return "Advantage player2";
     }
-    
-    if (p1Score > p2Score && p2Score >= 3)
-    {
-        score = "Advantage player1";
-    }
-    
-    if (p2Score > p1Score && p1Score >= 3)
-    {
-        score = "Advantage player2";
-    }
-    
-    if (p1Score>=4 && p2Score>=0 && (p1Score-p2Score)>=2)
-    {
-        score = "Win for player1";
-    }
-    if (p2Score>=4 && p1Score>=0 && (p2Score-p1Score)>=2)
-    {
-        score = "Win for player2";
-    }
-    return score;
-    
+  }
+
+  std::string P1res = regularScore(p1Score);
+  std::string P2res = regularScore(p2Score);
+  return P1res + "-" + P2res;
+
 }
